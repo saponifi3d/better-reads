7dd6ec949e9cfe8ddb32ed09bd558ea241c45bd6
Josh Callender <j@callender.me>
2018-01-07 17:02:59 -0800

    fix parcel to build correctly

--- components/App/index.jsx
+++ components/App/index.jsx
@@ -2,6 +2,8 @@ import React, { Component, Fragment } from 'react';
 import { Header } from '../Header';
 import styles from './App.css';
 
+console.log({ styles });
+
 export class App extends Component {
     render() {
         return (
